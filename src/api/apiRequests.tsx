148cd31fdfedddb81fffc2c151c46357f0eeec22
blksy <79380937+blksy@users.noreply.github.com>
2024-10-25 18:01:31 +0200

    imported interfaces to apiRequests.tsx and added response types

--- src/api/apiRequests.tsx
+++ src/api/apiRequests.tsx
@@ -1,34 +1,52 @@
 import axios from "axios";
+import {
+  Character,
+  Film,
+  Planet,
+  Species,
+  Starship,
+  Vehicle,
+} from "../interfaces/interfaces";
 
 const API_BASE_URL = "https://swapi.dev/api/";
 
-export const fetchMovies = async () => {
-  const { data } = await axios.get(`${API_BASE_URL}films`);
+export const fetchMovies = async (): Promise<Film[]> => {
+  const { data } = await axios.get<{ results: Film[] }>(`${API_BASE_URL}films`);
   return data.results;
 };
 export const fetchMovieDetails;
-export const fetchCharacters = async () => {
-  const { data } = await axios.get(`${API_BASE_URL}people`);
+export const fetchCharacters = async (): Promise<Character[]> => {
+  const { data } = await axios.get<{ results: Character[] }>(
+    `${API_BASE_URL}people`
+  );
   return data.results;
 };
 export const fetchCharacterDetails;
-export const fetchPlanets = async () => {
-  const { data } = await axios.get(`${API_BASE_URL}planets`);
+export const fetchPlanets = async (): Promise<Planet[]> => {
+  const { data } = await axios.get<{ results: Planet[] }>(
+    `${API_BASE_URL}planets`
+  );
   return data.results;
 };
 export const fetchPlanetDetails;
-export const fetchSpecies = async () => {
-  const { data } = await axios.get(`${API_BASE_URL}species`);
+export const fetchSpecies = async (): Promise<Species[]> => {
+  const { data } = await axios.get<{ results: Species[] }>(
+    `${API_BASE_URL}species`
+  );
   return data.results;
 };
 export const fetchSpeciesDetails;
-export const fetchVehicles = async () => {
-  const { data } = await axios.get(`${API_BASE_URL}vehicles`);
+export const fetchVehicles = async (): Promise<Vehicle[]> => {
+  const { data } = await axios.get<{ results: Vehicle[] }>(
+    `${API_BASE_URL}vehicles`
+  );
   return data.results;
 };
 export const fetchVehicleDetails;
-export const fetchStarships = async () => {
-  const { data } = await axios.get(`${API_BASE_URL}starships`);
+export const fetchStarships = async (): Promise<Starship[]> => {
+  const { data } = await axios.get<{ results: Starship[] }>(
+    `${API_BASE_URL}starships`
+  );
   return data.results;
 };
 export const fetchStarshipDetails;
