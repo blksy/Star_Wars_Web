0e143933a17b3db3a591732683465c661f10bcbb
blksy <79380937+blksy@users.noreply.github.com>
2024-10-26 13:57:57 +0200

    added routes for each page and details

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+const ROUTES = {
+  home: "/",
+  login: "/login",
+  register: "/register",
+  characters: "/characters",
+  characterDetails: (id: number | string) => `/characters/${id}`,
+  planets: "/planets",
+  planetDetails: (id: number | string) => `/planets/${id}`,
+  movies: "/movies",
+  movieDetails: (id: number | string) => `/movies/${id}`,
+  species: "/species",
+  speciesDetails: (id: number | string) => `/species/${id}`,
+  starships: "/starships",
+  starshipDetails: (id: number | string) => `/starships/${id}`,
+  vehicles: "/vehicles",
+  vehicleDetails: (id: number | string) => `/vehicles/${id}`,
+};
+
+export default ROUTES;
