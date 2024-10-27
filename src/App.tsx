5605e15cb363976a4c730eb70d0c653aa8639b4d
blksy <79380937+blksy@users.noreply.github.com>
2024-10-27 16:35:11 +0100

    added QueryClientProvider and QueryClient to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryCache,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ROUTES from "./routes";
 import "./App.css";
 
@@ -20,9 +26,21 @@ const StarshipDetails = lazy(() => import("./pages/Species/SpeciesDetails"));
 const Vehicles = lazy(() => import("./pages/Species/SpeciesDetails"));
 const VehicleDetails = lazy(() => import("./pages/Species/SpeciesDetails"));
 
+const queryClient = new QueryClient({
+  queryCache: new QueryCache(),
+  defaultOptions: {
+    queries: {
+      staleTime: 60_000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools position="bottom" initialIsOpen={false} />
+      )}
       <BrowserRouter>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
@@ -62,7 +80,7 @@ function App() {
           </Routes>
         </Suspense>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/hooks/useCreateUser.ts
@@ -0,0 +1,6 @@
+interface User {
+  name: string;
+  email: string;
+  nickname: string;
+  password: string;
+}
