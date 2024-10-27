f9247f017e19c4491d74a4141eecfc0575a574bc
blksy <79380937+blksy@users.noreply.github.com>
2024-10-27 16:48:16 +0100

    created custom hook useCreateUser that checks whether user exists and attempts to create a new one if not

--- src/hooks/useCreateUser.ts
+++ src/hooks/useCreateUser.ts
@@ -1,6 +1,46 @@
-interface User {
-  name: string;
-  email: string;
-  nickname: string;
-  password: string;
+import { useMutation } from "@tanstack/react-query";
+import { supabase } from "../database/supabase";
+import { User } from "../interfaces/interfaces";
+
+const createUser = async (user: User) => {
+  //check if user exists
+  const { data: userWithUsername } = await supabase
+    .from("users")
+    .select("*")
+    .eq("nickname", user.username)
+    .single();
+
+  if (userWithUsername) {
+    throw new Error("User with that username already exists");
+  }
+
+  const { data, error: signUpError } = await supabase.auth.signUp({
+    email: user.email,
+    password: user.password,
+  });
+
+  if (signUpError) {
+    throw signUpError;
+  }
+  return data;
+};
+
+export default function useCreateUser(user: User) {
+  return useMutation(() => createUser(user), {
+    onSuccess: async (data) => {
+      const { data: insertData, error: insertError } = await supabase
+        .from("users")
+        .insert({
+          name: user.name,
+          username: user.username,
+          id: data.user.id,
+        });
+
+      if (insertError) {
+        throw insertError;
+      }
+
+      return insertData;
+    },
+  });
 }
--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,7 +1,6 @@
 export interface User {
-  id: string | number;
   name: string;
-  nickname: string;
+  username: string;
   email: string;
   password: string;
 }
